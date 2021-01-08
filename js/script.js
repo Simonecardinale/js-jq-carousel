10c3a962a5fb74b3e99a31eb11392af48fe580b9
Simone Cardinale <simonecardinale8@gmail.com>
2021-01-08 15:41:37 +0100

    left and right slide complete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
-    
+
+    $('.container > span > i.fa-chevron-right').click(function(){
+        var spostamento = $('.container img.active');
+        spostamento.removeClass('active');
+        spostamento.next().addClass('active');
+        console.log(spostamento);
+
+        if(spostamento.hasClass('last')) {
+            $('.container img.first').addClass('active');
+        };
+
+    });
+
+    $('.container > span > i.fa-chevron-left').click(function(){
+        var spostamento = $('.container img.active');
+        spostamento.removeClass('active');
+        spostamento.prev().addClass('active');
+        console.log(spostamento);
+
+        if(spostamento.hasClass('first')) {
+            $('.container img.last').addClass('active');
+        };
+        
+    });
 });
